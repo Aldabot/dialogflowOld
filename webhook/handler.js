ced36a9e397b4ba631f3a6b6e51c1f2fe69c8556
knowledge91 <dirkhornung91@gmail.com>
2018-02-07 09:27:57 +0100

    dotenv & webpack: copy .env

--- webhook/handler.js
+++ webhook/handler.js
@@ -1,6 +1,25 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+import dotenv from 'dotenv';
+import mysql from 'mysql';
+import Promise from 'bluebird';
+Promise.promisifyAll(require("mysql/lib/Connection").prototype); // Promisfy MySQL callback-hell
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+dotenv.config(); // load .env configs
+
+console.log(process.env.RDS_HOST);
+
+// reserve 10 MySQL connections in pool
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.RDS_HOST,
+    user: process.env.RDS_USER,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB
+});
+
+
+export const index = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
