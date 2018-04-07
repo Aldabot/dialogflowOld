44c6a3344affa2c5d6563b9b8cf057c7c4f93fb9
knowledge91 <dirkhornung91@gmail.com>
2018-04-07 18:10:49 +0200

    added lenders model, local test json path

--- handler.js
+++ handler.js
@@ -1,11 +1,12 @@
 'use strict';
 
-import dotenv from 'dotenv';
-
-
 class DialogflowV1 {
-    constructor() {
+    constructor(req) {
         this.platform = 'facebook';
+
+        console.log(req);
+        this.contexts = req.result.contexts;
+
         this.response = {
             messages: [
             ]
@@ -51,27 +52,73 @@ class DialogflowV1 {
     }
 }
 
+const lenders = [
+    {
+        name: 'Cetelem',
+        minAmount: 3000,
+        maxAmount: 50000
+    }, {
+        name: 'Cofidis',
+        minAmount: 4000,
+        maxAmount: 50000
+    }, {
+        name: 'Creditea',
+        minAmount: 100,
+        maxAmount: 3000
+    }, {
+        name: 'Quebueno',
+        minAmount: 50,
+        maxAmount: 300
+    }, {
+        name: 'Solcredito',
+        minAmount: 100,
+        maxAmount: 1000
+    }, {
+        name: 'Vivus',
+        minAmount: 50,
+        maxAmount: 300
+    }, {
+        name: 'Wonga',
+        minAmount: 50,
+        maxAmount: 300
+    }, {
+        name: 'Younited',
+        minAmount: 1000,
+        maxAmount: 40000
+    },
+]
+
+
+function aldaFinancingPrestamo(agent) {
+    const amount = agent.contexts[0].parameters.amount;
+    for (const lender of lenders) {
+        const { name, minAmount, maxAmount } = lender;
+        if(minAmount <= amount && amount <= maxAmount) {
+            agent.addTextMessage(name);
+        }
+    }
+}
+
 
 export const index = (event, context, callback) => {
     // console.log(JSON.stringify(JSON.parse(event.body), null, 4));
 
     const body = process.env.IS_LOCAL ? event.body : JSON.parse(event.body);
 
+    console.log(body);
     console.log(JSON.stringify(body, null, 4));
 
-    const dialogflowV1Response = {
-        speech: 'joojojo',
-        displayText: 'joojojo'
-    };
 
-    const agent = new DialogflowV1();
-    agent.addTextMessage('lol');
-    agent.addTextMessage('non');
-    agent.addQuickReplies('title', ['lol', 'ok', 'nice']);
-    agent.addCard('title', 'subtitle', 'https://www.w3schools.com/howto/img_forest.jpg', [{
-        postback: 'lol',
-        text: 'ok'
-    }]);
+    const agent = new DialogflowV1(body);
+    // agent.addTextMessage('lol');
+    // agent.addTextMessage('non');
+    // agent.addQuickReplies('title', ['lol', 'ok', 'nice']);
+    // agent.addCard('title', 'subtitle', 'https://www.w3schools.com/howto/img_forest.jpg', [{
+    //     postback: 'lol',
+    //     text: 'ok'
+    // }]);
+
+    aldaFinancingPrestamo(agent);
 
     const response = {
         statusCode: 200,
